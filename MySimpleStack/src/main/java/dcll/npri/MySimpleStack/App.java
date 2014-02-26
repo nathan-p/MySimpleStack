480fd155befeb3cf8a70a9c7e5705a478290f020
nathan <nathan.prior.31@gmail.com>
2014-02-26 10:25:01 +0100

    initial commit

---
+++ MySimpleStack/src/main/java/dcll/npri/MySimpleStack/App.java
@@ -0,0 +1,13 @@
+package dcll.npri.MySimpleStack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
